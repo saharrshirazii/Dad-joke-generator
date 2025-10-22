b3304377b6e7577cb1601aaca846db0c1bb0a096
saharrshirazii <saharr.shirazii@gmail.com>
2025-10-22 10:42:44 +0200

    I tried fetch API

---
+++ DAD-JOKE-GENERATOR/app.js
@@ -0,0 +1,50 @@
+// const jokeEl = document.getElementById('joke');
+// const jokeBtn = document.getElementById('jokeBtn');
+
+// generateJoke();
+
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             Accept: 'application/json',
+//         },
+//     };
+
+//     fetch('https://icanhazdadjoke.com/', config)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             jokeEl.innerHTML = data.joke;
+//         })
+//         .catch((error) => {
+//             console.error('Error fetching joke:', error);
+//             jokeEl.innerHTML = 'Oops! Something went wrong. Please try again later.';
+//         });
+// }
+
+// jokeBtn.addEventListener('click', generateJoke);
+
+
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+generateJoke();
+
+async function generateJoke() {
+    try{
+        const config = {
+            headers: {
+                Accept: 'application/json',
+            },
+        };
+        const res = await fetch ('https://icanhazdadjoke.com/', config)
+        const data = await res.json();
+        jokeEl.innerHTML = data.joke;   
+    }
+    catch(error){
+        console.error('Error fetching joke:', error);
+        jokeEl.innerHTML = 'Something went wrong.'; 
+        jokeEl.style.color = 'red';      
+} 
+}
+
+jokeBtn.addEventListener('click', generateJoke);
