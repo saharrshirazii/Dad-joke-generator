d476dac6ce2b52f0832963471876f6302cb6abf2
saharrshirazii <137921636+saharrshirazii@users.noreply.github.com>
2025-10-23 09:59:21 +0200

    Refactor generateJoke to async/await
    Refactor generateJoke function to use async/await and improve error handling.

--- DAD-JOKE-GENERATOR/app.js
+++ DAD-JOKE-GENERATOR/app.js
@@ -1,53 +1,56 @@
+// const jokeEl = document.getElementById('joke');
+// const jokeBtn = document.getElementById('jokeBtn');
+
+// generateJoke();
+
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             Accept: 'application/json',
+//         },
+//     };
+
+//     fetch('https://icanhazdadjoke.com/', config)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             jokeEl.innerHTML = data.joke;
+//         })
+//         .catch((error) => {
+//             console.error('Error fetching joke:', error);
+//             jokeEl.innerHTML = 'Oops! Something went wrong. Please try again later.';
+//         });
+// }
+
+// jokeBtn.addEventListener('click', generateJoke);
+
+
 const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
 
 generateJoke();
 
-function generateJoke() {
+async function generateJoke() {
     const config = {
         headers: {
             Accept: 'application/json',
         },
     };
 
-    fetch('https://icanhazdadjoke.com/', config)
-        .then((res) => res.json())
-        .then((data) => {
-            jokeEl.innerHTML = data.joke;
-        })
-        .catch((error) => {
-            console.error('Error fetching joke:', error);
-            jokeEl.innerHTML = 'Oops! Something went wrong. Please try again later.';
-        });
+    try {
+        const res = await fetch('https://icanhazdadjoke.com/', config);
+        const data = await res.json();
+        if (!res.ok){
+            throw new error ('Joke not found');
+        }
+
+
+        jokeEl.innerHTML = data.joke;
+    } catch (error) {
+        console.error('Error fetching joke:', error);
+        jokeEl.innerHTML = 'Something is wrong!';
+        jokeEl.style.color = 'red';
+    
+    }
 }
 
 jokeBtn.addEventListener('click', generateJoke);
-
-
-// const jokeEl = document.getElementById('joke');
-// const jokeBtn = document.getElementById('jokeBtn');
-
-// generateJoke();
-
-// async function generateJoke() {
-//     try{
-//         const config = {
-//             headers: {
-//                 Accept: 'application/json',
-//             },
-//         };
-//         const res = await fetch ('https://icanhazdadjoke.com/ss', config)
-//         if (!res.ok){
-//             throw new Error (`Response not ok ${res.status}`);
-//         }
-//         const data = await res.json();
-//         jokeEl.innerHTML = data.joke;   
-//     }
-//     catch(error){
-//         console.error('Error fetching joke:', error);
-//         jokeEl.innerHTML = error.message; 
-//         jokeEl.style.color = 'red';      
-// } 
-// }
-
-// jokeBtn.addEventListener('click', generateJoke);
